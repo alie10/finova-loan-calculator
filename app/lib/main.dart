fe158b60c3e34b4b53c792e2b0e828c2c589e250
ali elsaid <alielsaid519@gmail.com>
2026-02-20 01:24:06 +0200

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'core/app.dart';
-
-import 'package:flutter/material.dart';
 import 'core/ads/ad_service.dart';
-// باقي imports…
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await AdService.instance.initialize(); // ✅
+  await AdService.instance.initialize();
   runApp(const MyApp());
 }
