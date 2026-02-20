b616093954ddfde74bf8e5b91e48daf7f7f462bf
ali elsaid <alielsaid519@gmail.com>
2026-02-20 22:12:28 +0200

    Update ad_banner.dart

--- app/lib/widgets/ad_banner.dart
+++ app/lib/widgets/ad_banner.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-
 import '../core/ads/ad_ids.dart';
 
 class AdBanner extends StatefulWidget {
@@ -11,51 +10,58 @@ class AdBanner extends StatefulWidget {
 }
 
 class _AdBannerState extends State<AdBanner> {
-  BannerAd? _bannerAd;
-  bool _isLoaded = false;
+  BannerAd? _ad;
+  bool _loaded = false;
 
   @override
   void initState() {
     super.initState();
+    _load();
+  }
 
-    _bannerAd = BannerAd(
+  void _load() {
+    final ad = BannerAd(
       adUnitId: AdIds.banner,
       request: const AdRequest(),
-      size: AdSize.banner,
+      size: AdSize.banner, // 320x50 غالبًا
       listener: BannerAdListener(
         onAdLoaded: (ad) {
           if (!mounted) return;
-          setState(() => _isLoaded = true);
+          setState(() {
+            _ad = ad as BannerAd;
+            _loaded = true;
+          });
         },
-        onAdFailedToLoad: (ad, error) {
+        onAdFailedToLoad: (ad, err) {
           ad.dispose();
           if (!mounted) return;
           setState(() {
-            _bannerAd = null;
-            _isLoaded = false;
+            _ad = null;
+            _loaded = false;
           });
         },
       ),
-    )..load();
+    );
+
+    ad.load();
   }
 
   @override
   void dispose() {
-    _bannerAd?.dispose();
+    _ad?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final ad = _bannerAd;
-    if (!_isLoaded || ad == null) return const SizedBox.shrink();
+    if (!_loaded || _ad == null) return const SizedBox.shrink();
 
     return SafeArea(
       top: false,
       child: SizedBox(
-        width: ad.size.width.toDouble(),
-        height: ad.size.height.toDouble(),
-        child: AdWidget(ad: ad),
+        height: _ad!.size.height.toDouble(),
+        width: _ad!.size.width.toDouble(),
+        child: AdWidget(ad: _ad!),
       ),
     );
   }
