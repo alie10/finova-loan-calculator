1a15f96f8229348eb74ebbeeb3e59cc0d7dd3914
ali elsaid <alielsaid519@gmail.com>
2026-02-20 22:11:21 +0200

    Update ad_ids.dart

--- app/lib/core/ads/ad_ids.dart
+++ app/lib/core/ads/ad_ids.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/foundation.dart';
+
 class AdIds {
-  // ✅ Google TEST IDs (must show quickly if integration is correct)
-  static const String banner = 'ca-app-pub-3940256099942544/6300978111';
-  static const String interstitial = 'ca-app-pub-3940256099942544/1033173712';
+  AdIds._();
+
+  // ✅ Android AdMob App ID (الذي يحتوي على ~)
+  static const String androidAppId = 'ca-app-pub-1243924347643904~8738724931';
+
+  // ✅ Banner (Release = الحقيقي / Debug = test)
+  static const String banner = kReleaseMode
+      ? 'ca-app-pub-1243924347643904/5595345403'
+      : 'ca-app-pub-3940256099942544/6300978111';
+
+  // ✅ Interstitial (Release = الحقيقي / Debug = test)
+  static const String interstitial = kReleaseMode
+      ? 'ca-app-pub-1243924347643904/9597475578'
+      : 'ca-app-pub-3940256099942544/1033173712';
 }
