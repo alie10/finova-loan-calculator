63b6afc2cc9dfd7218f04b5c623981a4c48d0236
ali elsaid <alielsaid519@gmail.com>
2026-02-20 23:06:41 +0200

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,11 @@
+// app/lib/main.dart
 import 'package:flutter/material.dart';
+
 import 'core/app.dart';
 import 'core/ads/ad_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Ads (AdMob)
   await AdService.instance.initialize();
-
   runApp(const FinovaApp());
 }
