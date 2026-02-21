7804a2ef3dcefb4341d9dcfaca32286e6b4693ce
ali elsaid <alielsaid519@gmail.com>
2026-02-21 22:22:44 +0200

    Update ad_banner.dart

--- app/lib/widgets/ad_banner.dart
+++ app/lib/widgets/ad_banner.dart
@@ -4,14 +4,7 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import '../core/ads/ad_service.dart';
 
 class AdBanner extends StatefulWidget {
-  const AdBanner({
-    super.key,
-    this.preferAli = true,
-    this.height = 60,
-  });
-
-  final bool preferAli;
-  final double height;
+  const AdBanner({super.key});
 
   @override
   State<AdBanner> createState() => _AdBannerState();
@@ -20,12 +13,38 @@ class AdBanner extends StatefulWidget {
 class _AdBannerState extends State<AdBanner> {
   BannerAd? _ad;
   bool _loaded = false;
-  String? _lastError;
 
   @override
   void initState() {
     super.initState();
     _load();
+  }
+
+  void _load() {
+    final unitId = AdService.instance.bannerUnitId();
+
+    final ad = BannerAd(
+      adUnitId: unitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _loaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          setState(() {
+            _ad = null;
+            _loaded = false;
+          });
+        },
+      ),
+    );
+
+    setState(() => _ad = ad);
+    ad.load();
   }
 
   @override
@@ -34,72 +53,14 @@ class _AdBannerState extends State<AdBanner> {
     super.dispose();
   }
 
-  Future<void> _load() async {
-    try {
-      await AdService.instance.initialize();
-
-      final unitId = AdService.instance.bannerUnitId(preferAli: widget.preferAli);
-
-      final ad = BannerAd(
-        adUnitId: unitId,
-        size: AdSize.banner,
-        request: AdService.instance.request(),
-        listener: BannerAdListener(
-          onAdLoaded: (ad) {
-            debugPrint('[AdBanner] Loaded: ${(ad as BannerAd).adUnitId}');
-            if (!mounted) return;
-            setState(() {
-              _ad = ad as BannerAd;
-              _loaded = true;
-              _lastError = null;
-            });
-          },
-          onAdFailedToLoad: (ad, err) {
-            debugPrint('[AdBanner] Failed: ${ad.adUnitId} => $err');
-            ad.dispose();
-            if (!mounted) return;
-            setState(() {
-              _ad = null;
-              _loaded = false;
-              _lastError = err.message;
-            });
-          },
-          onAdImpression: (ad) {
-            debugPrint('[AdBanner] Impression: ${ad.adUnitId}');
-          },
-        ),
-      );
-
-      await ad.load();
-    } catch (e) {
-      debugPrint('[AdBanner] Exception: $e');
-      if (!mounted) return;
-      setState(() {
-        _ad = null;
-        _loaded = false;
-        _lastError = e.toString();
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    // أثناء الإنتاج: لو ما اتحملش، نخفي المساحة عشان ما نزعّج المستخدم
-    if (!_loaded || _ad == null) {
-      // لو عايز تظهر debug للمراجعة فقط:
-      // return Text('Ad not loaded: ${_lastError ?? ''}');
-      return const SizedBox.shrink();
-    }
+    if (_ad == null || !_loaded) return const SizedBox.shrink();
 
     return SizedBox(
-      height: widget.height,
-      child: Center(
-        child: SizedBox(
-          width: _ad!.size.width.toDouble(),
-          height: _ad!.size.height.toDouble(),
-          child: AdWidget(ad: _ad!),
-        ),
-      ),
+      width: _ad!.size.width.toDouble(),
+      height: _ad!.size.height.toDouble(),
+      child: AdWidget(ad: _ad!),
     );
   }
 }
